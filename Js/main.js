c67fe8629ac5c4da8d10d41eab5430b3ce438b30
serhidalgra <serhidalgra@gmail.com>
2024-08-26 17:14:00 -0600

    Cambios que no se hicieron... solo hacer espacio en lineas

--- Js/main.js
+++ Js/main.js
@@ -5,27 +5,6 @@ var visible_menu = false;
 let menu = document.getElementById("nav");
 let nav_bar = document.getElementById("nav-bar");
 let links = document.querySelectorAll("nav a");
-/*
-
-function showHideMenu() {
-    if (visible_menu == false) {              //if menu hide
-        menu.style.display = "block";
-        visible_menu = true;
-    } else {
-        menu.style.display = "none";
-        visible_menu = false;
-    }
-}
-
-//I hide the menu when I choose an option
-let links = document.querySelectorAll("nav a");
-for (var x = 0; x < links.length; x++) {
-    links[x].onclick = function () {
-        menu.style.display = "none";
-        visible_menu = false;
-    }
-}
-*/
 
 function showHideMenu() {
     if (visible_menu == false) {
@@ -38,9 +17,7 @@ function showHideMenu() {
         visible_menu = false;
     }
 
-
-
-    // Agregar un event listener para cerrar el menú si se hace clic fuera de él
+    //   Agregar un event listener para cerrar el menú si se hace clic fuera de él
     document.addEventListener("click", function (event) {
         var target = event.target;
         if (!menu.contains(target) && target != nav_bar) {
@@ -58,9 +35,6 @@ function showHideMenu() {
         }
     });
 }
-
-
-
 
 
 //-------------------------------------------------------------------
